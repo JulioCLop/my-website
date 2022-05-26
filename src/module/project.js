008e367da46db63a73f4ed1d929eb775ff8fc7cb
Juliolopez <joeyclopez88@gmail.com>
2022-05-26 17:01:35 -0600

    change content of project

--- src/module/project.js
+++ src/module/project.js
@@ -1,5 +1,4 @@
 
-
 const projects = [
     {
         projectName: 'Virtual Birthday Card',
@@ -7,91 +6,65 @@ const projects = [
         projectLink: 'https://github.com/JulioCLop/VirtualBirthdayCard',
         projectLanguage: "JavaScript",
       
-      
     },{
         projectName: 'Rock, Paper, Scissors',
         projectText: ' Simple rock, paper, scissors project written in pure JavaScript.',
         projectLink: 'https://github.com/JulioCLop/rpsgame',
         projectLanguage: "JavaScript",
-    
-       
     },{
         projectName: 'Drum kit App',
         projectText: 'Go Ahead and play with my Drum kit',
         projectLink: 'https://github.com/JulioCLop/drumkit',
         projectLanguage: "JavaScript",
-    
-        
     }, {
-       
         projectName: 'Dad Jokes App',
         projectText: 'Check out the best dad jokes.',
         projectLink: 'https://github.com/JulioCLop/Dad-Jokes-App',
-        projectLanguage: " ReactJS",
-   
-        
+        projectLanguage: " React.js", 
     }, {
-       
         projectName: 'Todo List',
         projectText: ' Ideal Todo List with simple functionalities. ',
         projectLink: 'https://github.com/JulioCLop/React-Todo-App',
-        projectLanguage: "ReactJS",
-
+        projectLanguage: "React.js",
         
     }, {
-       
         projectName: 'Lights Out',
         projectText: 'The object of the game is to turn all the lights out on the grid.',
         projectLink: 'https://github.com/JulioCLop/lights-out',
         projectLanguage: "JavaScript",
-   
        
     }, {
-      
         projectName: 'Hangman',
         projectText: ' Guess letters one at a time to solve the word puzzle',
         projectLink: 'https://github.com/JulioCLop/hangman-app',
-        projectLanguage: "ReactJS",
-
+        projectLanguage: "React.js",
        
     }, {
-       
         projectName: 'e-Commerce Website',
         projectText: 'e-commerce clothing website',
         projectLink: 'https://github.com/JulioCLop/Mosters-rolodex',
-        projectLanguage: "ReactJS",
-     
-       
+        projectLanguage: "React.js",
     },{
         projectName: 'Landing Page Website',
         projectText: 'Travel with Tim and Save!',
         projectLink: 'https://github.com/JulioCLop/travel-agency',
         projectLanguage: "HTML, CSS",
-  
-       
     },{
         projectName: 'Tic,Tac,Toe',
         projectText: 'Who is feeling lucky, let\'s play!',
         projectLink: 'https://github.com/JulioCLop/Tic-Tac-Toe-Game',
         projectLanguage: "JavaScript",
-  
-       
     },
     {
         projectName: 'Expense App',
         projectText: 'keeping Track of your money!',
         projectLink: 'https://github.com/JulioCLop/expense-app',
         projectLanguage: "React.js",
-     
-       
     },{
         projectName: 'Image Recoginition',
         projectText: 'Let\'s find your face on the picture!',
         projectLink: 'https://github.com/JulioCLop/image--recognition',
         projectLanguage: "React.js",
-     
-       
-    
     },{
         projectName: 'Travel Agency',
         projectText: 'Let\'s travel the world',
@@ -115,14 +88,15 @@ const projects = [
         projectText: 'Check out all these monsters!',
         projectLink: 'https://github.com/JulioCLop/Mosters-rolodex',
         projectLanguage: "React.js",
+
     },{
         projectName: 'Hangman',
         projectText: 'Who is up for a game?',
         projectLink: 'https://github.com/JulioCLop/hangman-app',
         projectLanguage: "React.js",
+
     }
-    
-]
+];
 
 
 export default projects;
