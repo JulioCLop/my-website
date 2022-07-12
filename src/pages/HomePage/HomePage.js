3025db2f082b6d474e9265c0be38deecf1c2cff4
Juliolopez <joeyclopez88@gmail.com>
2022-07-11 22:02:04 -0600

    made changes to homepage image, and layout

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,44 +2,57 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-import Logo from '../../assets/LoadingPage/newLogoNoBackround.png';
+import Logo from "../../assets/LoadingPage/newLogoNoBackround.png";
 
-import {  useMediaQuery } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { MdArrowForwardIos } from 'react-icons/md';
+import { MdArrowForwardIos } from "react-icons/md";
+import {BsArrowRightCircle} from 'react-icons/bs';
 
 import classes from "./HomePage.module.css";
-
-
 
 const HomePage = () => {
   const theme = useTheme();
   const matchesMD = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <React.Fragment> 
+    <React.Fragment>
       <main className={classes["home-page"]}>
-      <div style={{backgroundImage: `url(${Logo})`}} className={classes.logo} />
+        <div className={classes.imageTwo} />
+        <div
+          style={{ backgroundImage: `url(${Logo})` }}
+          className={classes.logo}
+        />
         <section className={classes["section-flex"]}>
-         <article>
-         <div>
-           <h1>Next Level</h1>
-           <h1>Front End Developer</h1>
-         </div>
-         <div>
-           <p>I'm a Front-end Developer, and I build anything from mobile to web apps.</p>
-         </div>
-         </article>
-         <article>
-         <div id={classes.container}>
-           <Link to='/about'>Let's get started</Link>
-         </div>
-         </article>
+          <div className={classes["section-flex-center"]}>
+            <article>
+              <h1>Front End Developer</h1>
+                <h3>Building Amazing React Applications</h3>
+              <p>
+                I'm a Front-end Developer, and I build anything from mobile to
+                web apps.
+              </p>
+            </article>
+            <article>
+              <div>
+               <Link to="/about"><BsArrowRightCircle className={classes.arrowIcon} />  | Let's get started</Link>
+              </div>
+            </article>
+          </div>
         </section>
-      <div className={`${!matchesMD  ? classes['sideLink-mobile'] : classes.sideLink} }`}><MdArrowForwardIos className={classes.arrowRight}/><Link to="/contact"><small>Let's get to talking</small></Link></div>
-    </main>
+        <div
+          className={`${
+            !matchesMD ? classes["sideLink-mobile"] : classes.sideLink
+          } }`}
+        >
+          <MdArrowForwardIos className={classes.arrowRight} />
+          <Link to="/contact">
+            <small>Let's get to talking</small>
+          </Link>
+        </div>
+      </main>
     </React.Fragment>
   );
-}
+};
 
 export default React.memo(HomePage);
