fa479787d6c6465f12081768f981763bfe4a1be1
Juliolopez <joeyclopez88@gmail.com>
2022-07-14 21:46:29 -0600

    made changes to home page responsiveness

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,52 +1,42 @@
-import React, { useContext, useRef} from "react";
-
+import React, { useContext, useRef } from "react";
 
 import { ThemeContext } from "../../context/Theme.Contexts";
 
 import ScrollBtn from "../../components/UI/Button/ScrollBtn";
 
-import classes from  "./AboutPage.module.css";
+import classes from "./AboutPage.module.css";
 import MainHomePage from "../HomePage/MainHomePage";
 import { ContainerIntro } from "../../components/AboutPage_Components/ContainerIntro";
 import SmallDesc from "../../components/AboutPage_Components/SmallDesc";
 import SkillSetSection from "../../components/AboutPage_Components/SkillSetSection";
 import Worked from "../../components/AboutPage_Components/Worked";
-import {BiArrowToTop} from 'react-icons/bi';
 import CVSection from "../../components/AboutPage_Components/CVSection";
 
-
-const  AboutPage = () => {
-  const { darkMode} = useContext(ThemeContext);
+const AboutPage = () => {
+  const { darkMode } = useContext(ThemeContext);
   const elementTitle = useRef();
   const topElement = useRef();
- 
 
-  
+  const handleTopClick = () => {
+    topElement.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-const handleTopClick = () => {
-  topElement.current.scrollIntoView({behavior: 'smooth'})
-};
-
- 
-  
   return (
-  <React.Fragment>
-   <span ref={topElement}/>
-  <MainHomePage >
-    <div style={{height: '50px'}} ref={elementTitle} />
-    <div  className={classes["about-Page" ]} >
-     <ContainerIntro/>
-     <SmallDesc/>
-     <SkillSetSection/>
-     <Worked/>
-      <CVSection/>
-    </div>
-    <ScrollBtn  onClick={handleTopClick}>
-     Top
-     </ScrollBtn>
-    </MainHomePage>
+    <React.Fragment>
+      <span ref={topElement} />
+      <MainHomePage>
+        <div style={{ height: "50px" }} ref={elementTitle} />
+        <div className={classes["about-Page"]}>
+          <ContainerIntro />
+          <SmallDesc />
+          <SkillSetSection />
+          <Worked />
+          <CVSection />
+        </div>
+        <ScrollBtn onClick={handleTopClick}>Top</ScrollBtn>
+      </MainHomePage>
     </React.Fragment>
   );
-}
+};
 
 export default React.memo(AboutPage);
